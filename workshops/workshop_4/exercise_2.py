143f2c5a1db6480b966935719147a0e421c5c1c3
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-01 22:56:01 -0500

    exercise_2 finished

---
+++ workshops/workshop_4/exercise_2.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 22:00:57 2022
+
+@author: Diego Sanmiguel
+"""
+
+def traductor_de_frases_a_español(frase, diccionario):
+    frase_list = frase.split()
+    frase_list_traducida = []
+    frase_en_español = ""
+
+    for palabra in frase_list:        
+        frase_list_traducida.append(diccionario[palabra])
+        
+    frase_en_español = " ".join(frase_list_traducida)
+        
+    return frase_en_español    
+    
+
+dictionary_example = {
+	"gods": "dioses",
+	"thank": "gracias",
+	"I": "yo",
+	"for": "por",
+	"my": "mi",
+	"soul": "alma",
+    "am": "soy/estoy",
+    "the": "el/la",
+    "owner": "dueño",
+    "of": "de",
+    "destiny": "destino",
+    "captain": "capitan",
+    "love": "amor",
+    "is": "es",
+    "more": "más",
+    "powerful": "poderoso",
+    "than": "que",
+    "fear": "miedo",
+    "beautiful": "hermoso",
+    "better": "mejor",
+    "ugly": "feo",
+    "Simple": "simple",
+    "complex": "complejo",
+    "complicated":"complicado"}
+
+frases = ["I thank gods for my soul", "I am the captain of my soul", 
+          "love is more powerful than fear", "beautiful is better than ugly",
+          "Simple is better than complex", "complex is better than complicated"]
+
+for frase in frases:    
+    print("las traducción de la frase --> {} es: ".format(frase) + "\n" + traductor_de_frases_a_español(frase, dictionary_example))
+    print("-"*50)
+    
+
+
+
+
+
+
