60d71cba38c3c8696c90cbe029b97175e39befd2
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-08 19:21:13 -0500

    menu option 1 and 2

--- workshops/workshop_5/exercise_1.py
+++ workshops/workshop_5/exercise_1.py
@@ -5,6 +5,33 @@ Created on Sat May  7 16:57:16 2022
 @author: Diego Sanmiguel
 """
 
+def consultar_todo_el_menu(menu):
+    for item_menu in menu:        
+        print(item_menu)
+        print("-"*50)
+        
+def generador_id(menu):
+    list_id = []
+    cont = 0
+    mayor = "0"
+    for item_menu in menu:
+        list_id.append(item_menu[0].split("ID"))    
+        num_id = list_id[cont][1]
+        cont += 1
+        if num_id > mayor:
+            mayor = num_id
+    
+    nuevo_id = int(mayor) + 1    
+            
+    return "ID" + str(nuevo_id)
+        
+def agregar_menu(menu, id_, tipo_plato, plato):
+    nuevo_menu = []
+    nuevo_menu.append(id_)
+    nuevo_menu.append(tipo_plato)
+    nuevo_menu.append(plato)    
+    menu.append(nuevo_menu)
+
 def consultar_por_tipo_plato(menu, tipo_plato):    
     for item_menu in menu:
         if tipo_plato == item_menu[1]:            
@@ -56,7 +83,7 @@ def iniciar_aplicacion():
 def mostrar_menu_aplicacion(menu) -> bool:
     
     print("Menu de opciones")
-    print(" 1 - Consultar Menú")
+    print(" 1 - Consultar menú")
     print(" 2 - Agregar nuevo elemento al menú")
     print(" 3 - Consultar opciones de menú por tipo de plato")
     print(" 4 - Consultar platos por nombre")
@@ -68,17 +95,27 @@ def mostrar_menu_aplicacion(menu) -> bool:
     continuar_ejecutando = True
 
     if opcion_elegida == "1":
-        pass
-    elif opcion_elegida == "2":
-        pass
+        consultar_todo_el_menu(menu)
+        
+    elif opcion_elegida == "2":        
+        
+        id_ = generador_id(menu)
+        tipo_plato = input("Ingrese el tipo de plato que desea agregar: ")
+        plato = input("Ingrese el plato que corresponde al tipo de plato agregado: ")
+        
+        agregar_menu(menu, id_, tipo_plato, plato)        
+        
     elif opcion_elegida == "3":        
         tipo_plato = input("Digite el tipo de menú que desa consultar: ")
         consultar_por_tipo_plato(menu, tipo_plato)
+        
     elif opcion_elegida == "4":
         nombre_plato = input("Digite el nombre del plato que desea consultar: ")
         consultar_por_nombre_plato(menu, nombre_plato)
+        
     elif opcion_elegida == "5":
         pass
+    
     elif opcion_elegida == "6":
         continuar_ejecutando = False
     else:
