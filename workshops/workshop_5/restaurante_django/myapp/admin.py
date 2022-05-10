ecc645a660d14eaec038fb5e197bcf38cd0ac1a7
yurley <yurs.ksf1@gmail.com>
2022-05-09 20:01:32 -0500

    implementar filtro y busqueda en django admin

--- workshops/workshop_5/restaurante_django/myapp/admin.py
+++ workshops/workshop_5/restaurante_django/myapp/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Menu
 
 # Register your models here.
-admin.site.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_filter = ('type_dish',)
+    search_fields = ('name',)
+
+
+admin.site.register(Menu, MenuAdmin)
