ecd0f194c36bdf60d9c4ddd96ad98b141629aa9c
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-14 09:26:49 -0500

    exersice with templates

--- proyecto/mysite/proyecto/urls.py
+++ proyecto/mysite/proyecto/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from proyecto import views
 from django.views.generic import TemplateView
 
+app_name ="app"
+
 urlpatterns = [    
     path("Listjuegos/", views.listar_juegos), 
     path("", views.index, name="index"),    
-    path('juegadores/', TemplateView.as_view(template_name="jugadores.html"), name="jugadores"),
+    path('jugadores/', TemplateView.as_view(template_name="jugadores.html"), name="jugadores"),
     path('juegos/', TemplateView.as_view(template_name="juegos.html"), name="juegos"),    
+    path('italia/', TemplateView.as_view(template_name="itali.html"), name="italia"),
+    path('japan/', TemplateView.as_view(template_name="japan.html"), name="japan"),
 ]
 
