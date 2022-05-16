837149e76d27c76823db83605c4fef9853a0b0c7
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-16 18:25:54 -0500

    new paths

--- workshops/workshop_6/exercise_1/appMenu/urls.py
+++ workshops/workshop_6/exercise_1/appMenu/urls.py
@@ -5,5 +5,8 @@ from django.views.generic import TemplateView
 app_name = "app"
 
 urlpatterns = [
- path("", views.index, name="index")
+ path("", views.index, name="index"),
+ path('citas', TemplateView.as_view(template_name="citas.html"), name="citas"),
+ path('pacientes', TemplateView.as_view(template_name="pacientes.html"), name="pacientes"),
+ path('doctores', TemplateView.as_view(template_name="doctores.html"), name="doctores")
 ]
