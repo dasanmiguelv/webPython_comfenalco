910845d7c9c616b524048cf0c5c1d889432ce501
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-16 07:05:30 -0500

    Filters and search field

--- workshops/workshop_6/exercise_1/appMenu/admin.py
+++ workshops/workshop_6/exercise_1/appMenu/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from .models import Cita_medica, Paciente
 from .models import Doctor
 
+class PacienteAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
 
+class DoctorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
 
-
+class Cita_medicaAdmin(admin.ModelAdmin):
+    list_filter = ['fecha']
 
 # Register your models here.
-admin.site.register(Paciente)
-admin.site.register(Doctor)
-admin.site.register(Cita_medica)
+admin.site.register(Paciente, PacienteAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Cita_medica, Cita_medicaAdmin)
