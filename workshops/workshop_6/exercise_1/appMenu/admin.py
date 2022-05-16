e2d156fafe00c5595b2694688f0f35dff3038094
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-15 21:33:47 -0500

    new model cita_medica

--- workshops/workshop_6/exercise_1/appMenu/admin.py
+++ workshops/workshop_6/exercise_1/appMenu/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Paciente
+from .models import Cita_medica, Paciente
 from .models import Doctor
+
+
+
+
 
 # Register your models here.
 admin.site.register(Paciente)
 admin.site.register(Doctor)
+admin.site.register(Cita_medica)
--- workshops/workshop_6/exercise_1/appMenu/models.py
+++ workshops/workshop_6/exercise_1/appMenu/models.py
@@ -7,8 +7,23 @@ class Paciente(models.Model):
     apellido = models.CharField(max_length=200)
     fecha_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
 
+    def __str__(self):
+        return "{} - {} - {}".format(self.nombre, self.apellido, self.fecha_nacimiento)
+
 
 class Doctor(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)    
+    apellido = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{} - {}".format(self.nombre, self.apellido)    
+
+class Cita_medica(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    ubicacion = models.CharField(max_length=200)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return "{} -  {} - {} -  {}".format(self.paciente.nombre, self.doctor.nombre, self.ubicacion, self.fecha)
