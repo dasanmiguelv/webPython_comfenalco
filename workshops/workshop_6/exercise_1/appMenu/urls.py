d6499d7e54c0a31d3408e9c8a8bd1ced890aa13f
Diego Sanmiguel Vargas <dasanmiguel31@gmail.com>
2022-05-18 10:28:00 -0500

    list pacientes

--- workshops/workshop_6/exercise_1/appMenu/urls.py
+++ workshops/workshop_6/exercise_1/appMenu/urls.py
@@ -6,7 +6,7 @@ app_name = "appMenu"
 
 urlpatterns = [
  path("", views.index, name="home"),
- path('citas', views.list_citas, name="citas"),
+ path('listcitas', views.list_citas, name="listcitas"),
  path('citas', TemplateView.as_view(template_name="citas.html"), name="citas"),
  path('pacientes', TemplateView.as_view(template_name="pacientes.html"), name="pacientes"),
  path('doctores', TemplateView.as_view(template_name="doctores.html"), name="doctores")
